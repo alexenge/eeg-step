17a2a1a053435f5d17f8646af90f367b2a27e039
Alexander Enge <alexander.enge@hu-berlin.de>
2026-06-13 18:12:45 +0200

    Add/refactor group pipeline tests

--- tests/test_group.py
+++ tests/test_group.py
@@ -1,19 +1,51 @@
+from pathlib import Path
+
+from step.group import GroupPipeline
+from step.participant import ParticipantPipeline
+
+
 def test_group_pipeline(sample_group_pipeline):
     """Tests the GroupPipeline class."""
 
-    assert len(sample_group_pipeline.participant_pipelines) == 2
+    pipeline = sample_group_pipeline
+    assert isinstance(pipeline, GroupPipeline)
+
+    assert isinstance(pipeline.raw_files_, list)
+    assert all(isinstance(raw_file, (str, Path)) for raw_file in pipeline.raw_files_)
+
+    assert isinstance(pipeline.participant_ids, list)
+
+    assert isinstance(pipeline.log_files_, list)
+    assert all(isinstance(log_file, (str, Path)) for log_file in pipeline.log_files_)
+
+    assert isinstance(pipeline.besa_files_, list)
+    assert all(besa_file is None for besa_file in pipeline.besa_files_)
+
+    assert isinstance(pipeline.bad_channels_, list)
+    assert all(
+        bad_channels is None or isinstance(bad_channels, list)
+        for bad_channels in pipeline.bad_channels_
+    )
+
+    participant_pipelines = pipeline.participant_pipelines
+    assert isinstance(participant_pipelines, dict)
+    assert len(participant_pipelines) == 2
+    for participant_id, participant_pipeline in participant_pipelines.items():
+        assert participant_id in pipeline.participant_ids
+        assert isinstance(participant_pipeline, ParticipantPipeline)
 
 
 def test_group_pipeline_besa(sample_group_pipeline_besa):
     """Tests the GroupPipeline class with BESA/MSEC correction."""
 
-    assert len(sample_group_pipeline_besa.participant_pipelines) == 2
-    assert sample_group_pipeline_besa.participant_pipelines[
-        "09"
-    ].preproc_pipeline.bad_channels == ["Fp1", "PO8"]
-    assert (
-        sample_group_pipeline_besa.participant_pipelines[
-            "12"
-        ].preproc_pipeline.bad_channels
-        is None
-    )
+    pipeline = sample_group_pipeline_besa
+    assert isinstance(pipeline, GroupPipeline)
+
+    assert isinstance(pipeline.besa_files_, list)
+    assert all(isinstance(besa_file, (str, Path)) for besa_file in pipeline.besa_files_)
+
+    participant_pipelines = pipeline.participant_pipelines
+    assert len(participant_pipelines) == 2
+
+    assert participant_pipelines["09"].preproc_pipeline.bad_channels == ["Fp1", "PO8"]
+    assert participant_pipelines["12"].preproc_pipeline.bad_channels is None
